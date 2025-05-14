5f9abe7ec82e6a1e503887ce669a33111c9d7a35
AntonioGut <gutierrezcaceresantonio97@gmail.com>
2025-05-14 19:55:13 +0200

    Delete demo directory

--- demo/src/main/java/com/example/demo/controlador/Enum/EnumTiposDeCarne.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.controlador.Enum;
-
-public enum EnumTiposDeCarne {
-    Ternera, Cerdo, Pollo, Pavo
-}
--- demo/src/main/java/com/example/demo/controlador/Enum/TiposDeCorte.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.controlador.Enum;
-
-public enum TiposDeCorte {
-    Pechuga, Babilla, Muslo, Ala
-}
